54ac6e8f51df45480c0b5eaeb0477c4ff3849d73
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-06-10 15:18:47 +0800

    move api url in env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { parseCookies } from 'nookies';
 
-const api_url = 'http://127.0.0.1:8000/api/';
+const api_url = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/';
 
 const { jwt } = parseCookies();
 const axiosInstance = axios.create({
